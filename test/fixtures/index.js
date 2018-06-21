f8c446322246b90722a1ae622d6a5ceb298243c7
Jarrett Cruger <jcrugzz@gmail.com>
2018-06-21 12:01:00 -0400

    [wip] start integrationt tests

---
+++ test/fixtures/index.js
@@ -0,0 +1,15 @@
+
+exports.StatusFixture = {
+  pkg: 'my-package',
+  env: 'dev',
+  version: '1.0.0',
+  previousVersion: '0.9.0',
+  total: 10,
+  createDate: new Date(),
+  updateDate: new Date(),
+  complete: false
+};
+
+exports.StatusHeadFixture = {};
+exports.StatusCounterFixture = {};
+exports.StatusEvent = {};
