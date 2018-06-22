260d5c18483967cd7109363f727dbb27b2c9e0b2
Jarrett Cruger <jcrugzz@gmail.com>
2018-06-22 02:39:18 -0700

    [dist] fix all the things, add the docs, add license, add travis.yml

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid');
 
 exports.StatusFixture = {
   pkg: 'my-package',
@@ -10,6 +11,30 @@ exports.StatusFixture = {
   complete: false
 };
 
-exports.StatusHeadFixture = {};
-exports.StatusCounterFixture = {};
-exports.StatusEvent = {};
+exports.StatusHeadFixture = {
+  pkg: 'my-package',
+  env: 'dev',
+  version: '1.0.0',
+  previousVersion: '0.9.0',
+  total: 10,
+  createDate: new Date(),
+  updateDate: new Date(),
+  complete: false
+};
+
+exports.StatusCounterFixture = {
+  pkg: 'my-package',
+  env: 'dev',
+  version: '1.0.0'
+};
+
+exports.StatusEventFixture = {
+  pkg: 'my-package',
+  env: 'dev',
+  version: '1.0.0',
+  locale: 'en-US',
+  error: true,
+  message: 'Something is happening!',
+  details: 'More stuff about what is happening maybe',
+  eventId: uuid.v1()
+};
